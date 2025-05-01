4eb0fab7c099671fe55605ea38f635010eb12b6f
Stalin francis <educaysoft@gmail.com>
2025-04-30 23:09:46 -0500

    mejoras

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 session_start();
 
 // Incluir los controladores necesarios
-// require_once '../app/controllers/PersonaController.php';
+ require_once '../app/controllers/PersonaController.php';
 require_once '../app/controllers/SexoController.php';
-//require_once '../app/controllers/DireccionController.php';
-//require_once '../app/controllers/TelefonoController.php';
+require_once '../app/controllers/DireccionController.php';
+require_once '../app/controllers/TelefonoController.php';
 require_once '../app/controllers/EstadocivilController.php';
 
 $requestUri = $_SERVER["REQUEST_URI"];

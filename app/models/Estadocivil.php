647dc21dd4a998349eab58f4a04a386dd6ae98e8
Jhons23 <johan.quinde.bustos@utelvt.edu.ec>
2025-05-22 19:02:05 -0500

    Prueba

--- app/models/Estadocivil.php
+++ app/models/Estadocivil.php
@@ -1,7 +1,7 @@
 PHP
 
 <?php
-// Modelo EstadoCivil
+// Modelo EstadoCivil Por Jandry
 class EstadoCivil {
     private $conn;
     private $table_name = "estadocivil";
@@ -13,7 +13,7 @@ class EstadoCivil {
         $this->conn = $db;
     }
 
-    // Crear un nuevo estado civil
+    // Crear un nuevo estado civil Probando
     public function create() {
         try {
             $query = "INSERT INTO " . $this->table_name . " (nombre) VALUES (:nombre)";
